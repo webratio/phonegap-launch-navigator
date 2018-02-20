753c2842732cd1d962913c965327d1ec41eca286
Lorenzo Maghini <lorenzo.maghini@webratio.com>
2018-02-20 08:26:37 +0100

    Change of Google Maps API javascript loading. To avoid conflicts with the same mechanism used by the Google MAPS plugin
    Issue: #12779

--- webratio/emulation_stub.js
+++ webratio/emulation_stub.js
@@ -43,27 +43,26 @@ function createStubs() {
         console.log.apply(console, args);
     }
     
-    window.top["gmapinitialize"] = function() {
-        mapInit["mapinitialized"] = true;
-        mapInit["resolve"]();
-        google = window.top.google;
-    };
+    function initializeMaps(){
+	    window.top["gmapinitialize"] = function() {
+	        mapInit["mapinitialized"] = true;
+	        mapInit["resolve"]();
+	        google = window.top.google;
+	    };
+	
+	    mapInit = {
+	        mapinitialized: false
+	    };
 
-    mapInit = {
-        mapinitialized: false
-    };
-    mapInit["mappromise"] = new Promise(function(resolve) {
-        mapInit["resolve"] = resolve;
-    });
-
-	function loadScript() {
-        var script = document.createElement('script');
+	    mapInit["mappromise"] = new Promise(function(resolve) {
+	        mapInit["resolve"] = resolve;
+	    });
+	    
+    	var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '&callback=gmapinitialize';
         $('body').append(script);
     }
-    
-    setTimeout(loadScript, 400);
     
     return {
         LaunchNavigator: {
@@ -83,46 +82,50 @@ function createStubs() {
 				var destination = arguments[2];
 				var start = arguments[5];
 				
-                if (!directionsMap) {
-                    directionsMapView = initOpenMapForDirections();
-                    directionsService = new google.maps.DirectionsService();
-                    directionsDisplay = new google.maps.DirectionsRenderer();
-                    directionsMap = new google.maps.Map(directionsMapView[0]);
-                    directionsDisplay.setMap(directionsMap);
-                } else {
-                    wrapBackButton();
-                }
-
-                var request = {
-                    origin: start,
-                    destination: destination,
-                    travelMode: google.maps.TravelMode.DRIVING
-                };
-
-                directionsService.route(request, function(response, status) {
-                    if (status == google.maps.DirectionsStatus.OK) {
-                        directionsDisplay.setDirections(response);
-                    }
-                });
-
-                /* User clicks 'back' button */
-                $('#platform-events-fire-back-map').click(function(e) {
-
-                    /* Restores original 'back' button */
-                    $('#platform-events-fire-back-map').remove();
-                    $('#platform-events-fire-back').css("display", "initial");
-
-                    directionsMapView.hide('slide', {
-                        direction: 'left',
-                        duration: 250
-                    });
-                });
-
-                directionsMapView.show('slide', {
-                    direction: 'right',
-                    duration: 250
-                });
-				
+				if (window.top.google === undefined){
+					initializeMaps();
+				} else {
+					google = window.top.google;
+	                if (!directionsMap) {
+	                    directionsMapView = initOpenMapForDirections();
+	                    directionsService = new google.maps.DirectionsService();
+	                    directionsDisplay = new google.maps.DirectionsRenderer();
+	                    directionsMap = new google.maps.Map(directionsMapView[0]);
+	                    directionsDisplay.setMap(directionsMap);
+	                } else {
+	                    wrapBackButton();
+	                }
+	
+	                var request = {
+	                    origin: start,
+	                    destination: destination,
+	                    travelMode: google.maps.TravelMode.DRIVING
+	                };
+	
+	                directionsService.route(request, function(response, status) {
+	                    if (status == google.maps.DirectionsStatus.OK) {
+	                        directionsDisplay.setDirections(response);
+	                    }
+	                });
+	
+	                /* User clicks 'back' button */
+	                $('#platform-events-fire-back-map').click(function(e) {
+	
+	                    /* Restores original 'back' button */
+	                    $('#platform-events-fire-back-map').remove();
+	                    $('#platform-events-fire-back').css("display", "initial");
+	
+	                    directionsMapView.hide('slide', {
+	                        direction: 'left',
+	                        duration: 250
+	                    });
+	                });
+	
+	                directionsMapView.show('slide', {
+	                    direction: 'right',
+	                    duration: 250
+	                });
+				}
 			}
 		}
 	};
